972419168f3bd176cf7607fceca87b5777b67799
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-09-25 18:22:54 +0200

    Maak TennisClubContext

---
+++ TennisWebapplication/Data/TennisClubContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TennisWebapplication.Models
+{
+    public class TennisClubContext : DbContext
+    {
+        public TennisClubContext(DbContextOptions<TennisClubContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameResult> GameResults { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<MemberFine> MemberFines { get; set; }
+        public DbSet<MemberRole> MemberRoles { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Gender>().Property(i => i.Id).HasMaxLength(3);
+            modelBuilder.Entity<Gender>().Property(i => i.Name).HasMaxLength(10);
+        }
+    }
+}
