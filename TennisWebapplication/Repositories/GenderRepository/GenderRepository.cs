e11a4f4f3aabf7305f9902823664a0895530b94c
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-09-26 19:12:08 +0200

    Creëer gender repo implementatie

---
+++ TennisWebapplication/Repositories/GenderRepository/GenderRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisWebapplication.Models;
+
+namespace TennisWebapplication.Repositories.GenderRepository
+{
+    public class GenderRepository : IGenderRepository
+    {
+        private readonly TennisClubContext _context;
+
+        public GenderRepository(TennisClubContext context)
+        {
+        _context = context;
+        }
+
+        public IEnumerable<Gender> GetAllGenders()
+        {
+            return _context.Genders.ToList();
+        }
+
+        public Gender GetGenderById(int id)
+        {
+            return _context.Genders.FirstOrDefault(g => g.Id == id);
+        }
+    }
+}
