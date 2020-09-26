5c4ac591dd916a789292861aa6f8ffb95edcd73b
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-09-26 20:13:45 +0200

    Creëer league repo skelet

---
+++ TennisWebapplication/Repositories/LeagueRepository/LeagueRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisWebapplication.Repositories.LeagueRepository
+{
+    public class LeagueRepository : ILeagueRepository
+    {
+        public void GetAllLeagues()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetLeagueById(byte id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
