413a6b572fb42ab30f450ee9c5fb1ef6b4e996f8
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-09-26 19:50:54 +0200

    Creëer game repo contract

---
+++ TennisWebapplication/Repositories/GameRepository/IGameRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TennisWebapplication.Models;
+using TennisWebapplication.Repositories.MemberFineRepository;
+
+namespace TennisWebapplication.Repositories.GameRepository
+{
+    public interface IGameRepository : ISavable
+    {
+        void CreateGame(Game game);
+        void UpdateGame(Game game);
+        void DeleteGame(Game game);
+        void GetGamesByMemberAndDate(Member member, DateTime date);
+    }
+}
