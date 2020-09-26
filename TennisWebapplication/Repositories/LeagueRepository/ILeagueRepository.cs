b72cb92f9ec8e46a3fdf74cbb9fa48aa70c5d62f
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-09-26 20:13:35 +0200

    Creëer league repo contract

---
+++ TennisWebapplication/Repositories/LeagueRepository/ILeagueRepository.cs
@@ -0,0 +1,10 @@
+﻿using TennisWebapplication.Repositories.MemberFineRepository;
+
+namespace TennisWebapplication.Repositories.LeagueRepository
+{
+    public interface ILeagueRepository : ISavable
+    {
+        void GetAllLeagues();
+        void GetLeagueById(byte id);
+    }
+}
