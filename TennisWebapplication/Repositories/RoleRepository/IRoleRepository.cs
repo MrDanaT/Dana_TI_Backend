3006ed6f2e4496aafd60138fecb1a767088cba79
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-09-26 19:16:42 +0200

    Creëer role repo contract

---
+++ TennisWebapplication/Repositories/RoleRepository/IRoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisWebapplication.Models;
+
+namespace TennisWebapplication.Repositories.RoleRepository
+{
+    interface IRoleRepository
+    {
+        void SaveChanges();
+
+        void CreateRole(Role role);
+
+        void UpdateRole(Role role);
+    }
+}
