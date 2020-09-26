87ae7c66ea33b326194a10094159a161dd26fa1b
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-09-26 19:11:47 +0200

    Creëer gender repo contract

---
+++ TennisWebapplication/Repositories/GenderRepository/IGenderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisWebapplication.Models;
+
+namespace TennisWebapplication.Repositories.GenderRepository
+{
+    interface IGenderRepository
+    {
+        IEnumerable<Gender> GetAllGenders();
+        Gender GetGenderById(int id);
+    }
+}
