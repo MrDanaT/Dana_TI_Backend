b31ca5c4735e210778fb6e0ff1ef8b9bade961e0
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-09-26 19:11:55 +0200

    Creëer member repo contract

---
+++ TennisWebapplication/Repositories/MemberRepository/IMemberRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisWebapplication.Models;
+
+namespace TennisWebapplication.Repositories.MemberRepository
+{
+    interface IMemberRepository
+    {
+        void SaveChanges();
+
+        IEnumerable<Member> GetCommandByFederationNr(string federationNr);
+        IEnumerable<Member> GetCommandByFirstName(string firstName);
+        IEnumerable<Member> GetCommandByLastNameAndZipcode(string lastName, string zipcode);
+        IEnumerable<Member> GetCommandByCity(string city);
+        void CreateMember(Member member);
+        void UpdateMember(Member member);
+        void DeleteMember(Member member);
+    }
+}
