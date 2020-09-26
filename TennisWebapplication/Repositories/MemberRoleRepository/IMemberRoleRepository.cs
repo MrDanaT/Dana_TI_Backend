ae8c17e5eb1537cc6346aa1161193fa5992e5c72
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-09-26 19:36:53 +0200

    Creëer memberrole repo contract

---
+++ TennisWebapplication/Repositories/MemberRoleRepository/IMemberRoleRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisWebapplication.Models;
+
+namespace TennisWebapplication.Repositories.MemberRoleRepository
+{
+    internal interface IMemberRoleRepository
+    {
+        void SaveChanges();
+
+        void AssignMemberRole(MemberRole role);
+        void RetractMemberRole(MemberRole role);
+        void GetMembersRolesByRole(params Role[] role);
+        void GetMemberRolesByMember(Member member);
+    }
+}
