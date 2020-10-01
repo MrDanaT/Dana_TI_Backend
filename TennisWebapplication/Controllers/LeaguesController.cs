145e124acfa5516351539112ae32e2e37bd6c369
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-02 01:46:37 +0200

    Create LeaguesController.cs

---
+++ TennisWebapplication/Controllers/LeaguesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisWebapplication.Models;
+using TennisWebapplication.Repositories.GenderRepository;
+using TennisWebapplication.Repositories.LeagueRepository;
+
+namespace TennisWebapplication.Controllers
+{
+    public class LeaguesController : Controller
+    {
+        private readonly ILeagueRepository _repo;
+
+        public LeaguesController(ILeagueRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public ActionResult<IEnumerable<League>> GetAllLeagues()
+        {
+            IEnumerable<League> leagueItems = _repo.GetAllLeagues();
+
+            return Ok(leagueItems);
+        }
+    }
+}
