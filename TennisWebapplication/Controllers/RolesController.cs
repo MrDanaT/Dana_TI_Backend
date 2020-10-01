576d983398a5c10ac66ed1b3d517e47ac9316d37
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-02 01:46:39 +0200

    Create RolesController.cs

---
+++ TennisWebapplication/Controllers/RolesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TennisWebapplication.Models;
+using TennisWebapplication.Repositories.RoleRepository;
+
+namespace TennisWebapplication.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly IRoleRepository _repo;
+
+        public RolesController(IRoleRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public ActionResult<IEnumerable<Role>> GetAllRoles()
+        {
+            IEnumerable<Role> roleItems = _repo.GetAllRoles();
+
+            return Ok(roleItems);
+        }
+
+        // Test, vervangen door DTO's.
+        public ActionResult<Role> CreateRole(Role role)
+        {
+            _repo.CreateRole(role);
+            _repo.SaveChanges();
+
+            return Ok(role);
+        }
+
+        // Test, vervangen door DTO's.
+        public ActionResult<Role> UpdateRole(int id, Role role)
+        {
+            Role roleFromRepo = _repo.GetRoleById(id);
+
+            if (roleFromRepo == null)
+                return NotFound();
+
+            // TODO: do the changes here
+
+            _repo.UpdateRole(role);
+            _repo.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
