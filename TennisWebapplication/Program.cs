167a54d348b06bbc8069b34b525973dcd4399fd0
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-02 17:25:00 +0200

    Pas Program.cs aan naar nieuwe settings

--- TennisWebapplication/Program.cs
+++ TennisWebapplication/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using TennisClub.DAL;
 
 namespace TennisWebapplication
 {
@@ -7,16 +11,27 @@ namespace TennisWebapplication
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IWebHost host = CreateWebHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+                try
+                {
+                    TennisClubContext context = services.GetRequiredService<TennisClubContext>();
+                    // DataSeeder.Initialize(context);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
         }
     }
 }
