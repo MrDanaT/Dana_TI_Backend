5d5b362b33d0e7e9fb2e258c7b5adc5462d02796
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-04 23:53:32 +0200

    Maak gameresult controller af

---
+++ TennisClub.API/Controllers/GameResultsController.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisClub.BL.Entities;
+using TennisClub.DAL.Repositories.GameResultRepository;
+using TennisClub.DAL.Repositories.MemberRepository;
+using TennisClub.DTO.GameResult;
+
+namespace TennisClub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameResultsController : Controller
+    {
+        private readonly IGameResultRepository _repo;
+        private readonly IMapper _mapper;
+        private readonly IMemberRepository _memberRepo;
+
+        public GameResultsController(IGameResultRepository repo, IMapper mapper, IMemberRepository memberRepo)
+        {
+            _repo = repo;
+            _mapper = mapper;
+            _memberRepo = memberRepo;
+        }
+
+        // GET: api/gameresults
+        [HttpGet]
+        public ActionResult<IEnumerable<GameResultReadDTO>> GetAllGameResults()
+        {
+            IEnumerable<GameResult> gameResultItems = _repo.GetAllGameResults();
+
+            return Ok(_mapper.Map<IEnumerable<GameResultReadDTO>>(gameResultItems));
+        }
+
+        // GET: api/gameresults/5
+        [HttpGet("{id}", Name = "GetGameResultById")]
+        public ActionResult<GameResultReadDTO> GetGameResultById(int id)
+        {
+            GameResult gameResultItem = _repo.GetGameResultById(id);
+
+            if (gameResultItem != null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<GameResultReadDTO>(gameResultItem));
+        }
+
+        // POST: api/gameresults
+        [HttpPost]
+        public ActionResult<GameResultReadDTO> CreateGameResult(GameResultCreateDTO gameResultCreateDto)
+        {
+            GameResult gameResultModel = _mapper.Map<GameResult>(gameResultCreateDto);
+
+            _repo.CreateGameResult(gameResultModel);
+            _repo.SaveChanges();
+
+            GameResultReadDTO gameResultReadDto = _mapper.Map<GameResultReadDTO>(gameResultModel);
+
+            return CreatedAtRoute(nameof(GetGameResultById), new { gameResultReadDto.Id }, gameResultReadDto);
+        }
+
+        // PATCH: api/gameresults/5
+        [HttpPatch("{id}")]
+        public ActionResult PartialGameResultUpdate(int id, JsonPatchDocument<GameResultUpdateDTO> patchDoc)
+        {
+            GameResult gameResultModelFromRepo = _repo.GetGameResultById(id);
+
+            if (gameResultModelFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            GameResultUpdateDTO gameResultToPatch = _mapper.Map<GameResultUpdateDTO>(gameResultModelFromRepo);
+            patchDoc.ApplyTo(gameResultToPatch, ModelState);
+
+            if (!TryValidateModel(gameResultToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            _mapper.Map(gameResultToPatch, gameResultModelFromRepo);
+
+            _repo.UpdateGameResult(gameResultModelFromRepo);
+            _repo.SaveChanges();
+
+            return NoContent();
+        }
+
+        // GET: api/gameresults/bymemberid/5
+        [HttpGet("bymemberid/{id}")]
+        public ActionResult<GameResultReadDTO> GetGameResultsByMember(int id)
+        {
+            Member memberItem = _memberRepo.GetMemberById(id);
+            IEnumerable<GameResult> gameResultItems = _repo.GetGameResultsByMember(memberItem);
+
+            return Ok(_mapper.Map<GameResultReadDTO>(gameResultItems));
+        }
+    }
+}
