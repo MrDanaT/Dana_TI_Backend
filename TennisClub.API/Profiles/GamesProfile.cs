0f65967a3ed864c3fa72032c4d79e847644662c5
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-04 22:47:22 +0200

    Create GamesProfile.cs

---
+++ TennisClub.API/Profiles/GamesProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisClub.BL.Entities;
+using TennisClub.DTO.Game;
+
+namespace TennisClub.API.Profiles
+{
+    public class GamesProfile : Profile
+    {
+        public GamesProfile()
+        {
+            CreateMap<Game, GameReadDTO>();
+            CreateMap<GameReadDTO, Game>();
+            CreateMap<GameCreateDTO, Game>();
+            CreateMap<Game, GameCreateDTO>();
+        }
+    }
+}
