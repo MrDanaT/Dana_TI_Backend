286a5ccb497c4999e4c1c87d37b42381e0ce4b38
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-04 13:10:10 +0200

    Create RolesProfile.cs

---
+++ TennisClub.API/Profiles/RolesProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisClub.BL.Entities;
+using TennisClub.DTO.Role;
+
+namespace TennisClub.API.Profiles
+{
+    public class RolesProfile : Profile
+    {
+        public RolesProfile()
+        {
+            CreateMap<Role, RoleReadDTO>();
+            CreateMap<RoleCreateDTO, Role>();
+            CreateMap<RoleUpdateDTO, Role>();
+            CreateMap<Role, RoleUpdateDTO>();
+        }
+    }
+}
