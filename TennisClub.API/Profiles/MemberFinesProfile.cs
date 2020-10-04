22e89a1aeaa3b5e38ff98569363d28d7ff1faab3
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-04 21:20:08 +0200

    Create MemberFinesProfile.cs

---
+++ TennisClub.API/Profiles/MemberFinesProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisClub.BL.Entities;
+using TennisClub.DTO.MemberFine;
+
+namespace TennisClub.API.Profiles
+{
+    public class MemberFinesProfile : Profile
+    {
+        public MemberFinesProfile()
+        {
+            CreateMap<MemberFine, MemberFineReadDTO>();
+            CreateMap<MemberFineReadDTO, MemberFine>();
+            CreateMap<MemberFineCreateDTO, MemberFine>();
+            CreateMap<MemberFine, MemberFineCreateDTO>();
+        }
+    }
+}
