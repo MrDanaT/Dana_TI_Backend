3d2e60cf8d37c9aa047913c587404cf32884133a
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-09 18:35:45 +0200

    Vervolledig IRepo

---
+++ TennisClub.DAL/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TennisClub.DAL.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+        bool SaveChanges();
+    }
+}
