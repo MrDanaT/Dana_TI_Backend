9da532723576af8b8c088b158f28e4366420147e
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-11-02 14:58:31 +0100

    Maak gebruik van regions

--- TennisClub.UI/MainWindow.xaml.cs
+++ TennisClub.UI/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using TennisClub.Common.League;
+using TennisClub.Common.Role;
 using TennisClub.DAL.Entities;
 
 namespace TennisClub.UI
@@ -16,18 +19,130 @@ namespace TennisClub.UI
             InitializeComponent();
         }
 
-        private void GetRoles()
+        #region Roles
+
+        /*
+         * CRUD
+         */
+        private void CreateRole(object sender, AddingNewItemEventArgs e)
         {
-            Task<HttpResponseMessage> roles = WebAPI.GetCall("roles");
-            if (roles.Result.StatusCode == System.Net.HttpStatusCode.OK)
+            DataGrid itemsControl = RoleData;
+
+            RoleCreateDTO newRole = new RoleCreateDTO { Name = "lol" };
+            Task<HttpResponseMessage> response = WebAPI.PostCall("roles", newRole);
+
+            if (response.Result.StatusCode == System.Net.HttpStatusCode.Created)
             {
-                RoleData.ItemsSource = roles.Result.Content.ReadAsAsync<List<Role>>().Result;
+                System.Console.WriteLine("Gelukt");
+                itemsControl.ItemsSource = response.Result.Content.ReadAsAsync<List<object>>().Result;
+            }
+            else
+            {
+                System.Console.WriteLine("Niet gelukt!");
             }
         }
 
+        private void ReadRoles()
+        {
+            Task<HttpResponseMessage> result = WebAPI.GetCall("roles");
+            DataGrid itemsControl = RoleData;
+
+            if (result.Result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                itemsControl.ItemsSource = result.Result.Content.ReadAsAsync<List<RoleReadDTO>>().Result;
+            }
+            else
+            {
+                System.Console.WriteLine("Niet gelukt!");
+            }
+        }
+
+        private void UpdateRole()
+        {
+
+        }
+
+        private void DeleteRole()
+        {
+
+        }
+
+        /*
+         * Event Handlers
+         */
         private void GetRolesButton_Click(object sender, RoutedEventArgs e)
         {
-            GetRoles();
+            ReadRoles();
         }
+        private void RoleData_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            DeleteRole();
+        }
+
+        private void RoleData_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            UpdateRole();
+        }
+
+        #endregion
+
+        #region Leagues
+
+        /*
+         * CRUD
+         */
+        private void ReadLeagues()
+        {
+            Task<HttpResponseMessage> result = WebAPI.GetCall("leagues");
+            DataGrid itemsControl = LeagueData;
+
+            if (result.Result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                itemsControl.ItemsSource = result.Result.Content.ReadAsAsync<List<LeagueReadDTO>>().Result;
+            }
+            else
+            {
+                System.Console.WriteLine("Niet gelukt!");
+            }
+        }
+
+        /*
+         * Event Handlers
+         */
+        private void GetLeaguesButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReadLeagues();
+        }
+
+        #endregion
+
+        #region Genders
+        /*
+         * CRUD
+         */
+        private void ReadGenders()
+        {
+            Task<HttpResponseMessage> result = WebAPI.GetCall("leagues");
+            DataGrid itemsControl = GenderData;
+
+            if (result.Result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                itemsControl.ItemsSource = result.Result.Content.ReadAsAsync<List<object>>().Result;
+            }
+            else
+            {
+                System.Console.WriteLine("Niet gelukt!");
+            }
+        }
+
+        /*
+         * Event Handlers
+         */
+        private void GetGendersButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReadGenders();
+        }
+
+        #endregion
     }
 }
